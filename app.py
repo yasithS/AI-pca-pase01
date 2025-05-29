e50706c4bc3fa6ee576aea6a828a31e62a13e36b
Yasith Gunawardhana <gunawardhanayasith@gmail.com>
2025-05-29 09:21:21 +0530

    fix: minor naming issue

--- app.py
+++ app.py
@@ -148,7 +148,7 @@ def graphs():
 
 @app.errorhandler(404)
 def page_not_found(e):
-    return render_template('404.html'), 404
+    return render_template('error_404.html'), 404
 
 if __name__ == '__main__':
     app.run(debug=True)
